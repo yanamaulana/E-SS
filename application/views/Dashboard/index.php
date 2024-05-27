bfcb0309a208390224e96b7051f16ae9acf77d91
yanamaulana <51176753+yanamaulana@users.noreply.github.com>
2024-05-27 08:14:09 +0700

    Update index.php

--- application/views/Dashboard/index.php
+++ application/views/Dashboard/index.php
@@ -4,7 +4,7 @@
             <div class="container my-5">
                 <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
                     <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-                        <h1 class="display-4 fw-bold lh-1">Welcome to Samick Suport System.</h1>
+                        <h1 class="display-4 fw-bold lh-1">Welcome to Samick Support System.</h1>
                         <hr />
                         <p class="lead">Melalui musik sebagai bahasa universal,
                             Samick bertekad untuk meningkatkan kualitas kehidupan manusia. </p>
