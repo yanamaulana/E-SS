8ed6e1054d51cdaa49efd4ac25de8d43a0e03539
yanamaulana <51176753+yanamaulana@users.noreply.github.com>
2025-12-04 11:41:29 +0700

    Update CbrAppDirector.php

--- application/controllers/CbrAppDirector.php
+++ application/controllers/CbrAppDirector.php
@@ -97,7 +97,11 @@ class CbrAppDirector extends CI_Controller
 
     public function DT_List_To_Approve()
     {
+        // Mendapatkan data request (untuk DataTables)
         $requestData = $_REQUEST;
+        $username = $this->session->userdata('sys_sba_username');
+
+        // Kolom untuk pengurutan dan pencarian
         $columns = array(
             0 => 'TAccCashBookReq_Header.CBReq_No',
             1 => 'TAccCashBookReq_Header.CBReq_No',
@@ -117,100 +121,121 @@ class CbrAppDirector extends CI_Controller
             15 => 'First_Name',
             16 => 'Last_Update',
             17 => 'Update_By',
-            18 => 'TAccCashBookReq_Header.Acc_ID ',
+            18 => 'TAccCashBookReq_Header.Acc_ID',
             19 => 'TAccCashBookReq_Header.Approve_Date',
-
         );
-        $order  = $columns[$requestData['order']['0']['column']];
-        $dir    = $requestData['order']['0']['dir'];
-        $username = $this->session->userdata('sys_sba_username');
 
-        $sql = "SELECT  distinct TAccCashBookReq_Header.CBReq_No, Type, Document_Date, Document_Number, TAccCashBookReq_Header.Acc_ID, Descript, Amount, baseamount, curr_rate, Approval_Status, CBReq_Status, Paid_Status, Creation_DateTime, Created_By, First_Name AS Created_By_Name, Last_Update, Update_By, TAccCashBookReq_Header.Currency_Id, TAccCashBookReq_Header.Approve_Date, UserDivision
-        FROM TAccCashBookReq_Header
-        INNER JOIN TUserGroupL ON TAccCashBookReq_Header.Created_By = TUserGroupL.User_ID
-        INNER JOIN TUserPersonal ON TAccCashBookReq_Header.Created_By = TUserPersonal.User_ID
-        LEFT OUTER JOIN Ttrx_Cbr_Approval ON TAccCashBookReq_Header.CBReq_No = Ttrx_Cbr_Approval.CBReq_No
-        WHERE TAccCashBookReq_Header.Type='D'
-        AND TAccCashBookReq_Header.Company_ID = 2 
-        AND isNull(isSPJ,0) = 0
-        AND Approval_Status  = 3
-        AND CBReq_Status = 3
-        AND Ttrx_Cbr_Approval.CBReq_No IS NOT NULL
-        AND 
-        (
-            Ttrx_Cbr_Approval.AppvDirector_By = '$username' AND IsAppvDirector = 1 AND Status_AppvDirector = 0
-            AND ((IsAppvStaff = 0)          or (IsAppvStaff = 1 and Status_AppvStaff = 1))
-            AND ((IsAppvChief = 0)          or (IsAppvChief = 1 and Status_AppvChief = 1))
-            AND ((IsAppvAsstManager = 0)    or (IsAppvAsstManager = 1 and Status_AppvAsstManager = 1))
-            AND ((IsAppvManager = 0)        or (IsAppvManager = 1 and Status_AppvManager = 1))
-            AND ((IsAppvSeniorManager = 0)  or (IsAppvSeniorManager = 1 and Status_AppvSeniorManager = 1))
-            AND ((IsAppvGeneralManager = 0) or (IsAppvGeneralManager = 1 and Status_AppvGeneralManager = 1))
-            AND ((IsAppvAdditional = 0)     or (IsAppvAdditional = 1 and Status_AppvAdditional = 1)) 
-            AND ((IsAppvFinancePerson = 0)  or (IsAppvFinancePerson = 1 and Status_AppvFinancePerson = 1))
-        )             
-        OR 
-        (
-            AppvAdditional_By = '$username' AND IsAppvAdditional = 1 AND Status_AppvAdditional = 0 
-            AND ((IsAppvStaff = 0)         or (IsAppvStaff = 1 and Status_AppvStaff = 1))
-            AND ((IsAppvChief = 0)         or (IsAppvChief = 1 and Status_AppvChief = 1))
-            AND ((IsAppvAsstManager = 0)   or (IsAppvAsstManager = 1 and Status_AppvAsstManager = 1))
-            AND ((IsAppvManager) = 0       or (IsAppvManager = 1 and Status_AppvManager = 1))
-            AND ((IsAppvSeniorManager) = 0 or (IsAppvSeniorManager = 1 and Status_AppvSeniorManager = 1))
-            AND ((IsAppvGeneralManager) = 0 or (IsAppvGeneralManager = 1 and Status_AppvGeneralManager = 1))
-        )
-        
-         ";
-        // ORDER BY TAccCashBookReq_Header.Document_Date DESC,TAccCashBookReq_Header.CBReq_No DESC 
+        // Menentukan pengurutan
+        $order = $columns[$requestData['order']['0']['column']];
+        $dir = $requestData['order']['0']['dir'];
 
+        // 1. Kueri Dasar (Tanpa Batas, Urutan, dan Pencarian)
+        $sql = "SELECT DISTINCT 
+                    H.CBReq_No, H.Type, H.Document_Date, H.Document_Number, 
+                    H.Acc_ID, H.Descript, H.Amount, H.baseamount, H.curr_rate, 
+                    H.Approval_Status, H.CBReq_Status, H.Paid_Status, H.Creation_DateTime, 
+                    H.Created_By, P.First_Name AS Created_By_Name, H.Last_Update, H.Update_By, 
+                    H.Currency_Id, H.Approve_Date, GL.UserDivision
+                FROM TAccCashBookReq_Header H
+                INNER JOIN TUserGroupL GL ON H.Created_By = GL.User_ID
+                INNER JOIN TUserPersonal P ON H.Created_By = P.User_ID
+                LEFT OUTER JOIN Ttrx_Cbr_Approval A ON H.CBReq_No = A.CBReq_No
+                WHERE H.Type='D'
+                AND H.Company_ID = 2 
+                AND ISNULL(H.isSPJ,0) = 0
+                AND H.Approval_Status = 3
+                AND H.CBReq_Status = 3
+                AND A.CBReq_No IS NOT NULL
+                AND 
+                (
+                    (
+                        A.AppvDirector_By = '$username' AND A.IsAppvDirector = 1 AND A.Status_AppvDirector = 0
+                        AND ((A.IsAppvStaff = 0) OR (A.IsAppvStaff = 1 AND A.Status_AppvStaff = 1))
+                        AND ((A.IsAppvChief = 0) OR (A.IsAppvChief = 1 AND A.Status_AppvChief = 1))
+                        AND ((A.IsAppvAsstManager = 0) OR (A.IsAppvAsstManager = 1 AND A.Status_AppvAsstManager = 1))
+                        AND ((A.IsAppvManager = 0) OR (A.IsAppvManager = 1 AND A.Status_AppvManager = 1))
+                        AND ((A.IsAppvSeniorManager = 0) OR (A.IsAppvSeniorManager = 1 AND A.Status_AppvSeniorManager = 1))
+                        AND ((A.IsAppvGeneralManager = 0) OR (A.IsAppvGeneralManager = 1 AND A.Status_AppvGeneralManager = 1))
+                        AND ((A.IsAppvAdditional = 0) OR (A.IsAppvAdditional = 1 AND A.Status_AppvAdditional = 1)) 
+                        AND ((A.IsAppvFinancePerson = 0) OR (A.IsAppvFinancePerson = 1 AND A.Status_AppvFinancePerson = 1))
+                    )
+                    OR 
+                    (
+                        A.AppvAdditional_By = '$username' AND A.IsAppvAdditional = 1 AND A.Status_AppvAdditional = 0 
+                        AND ((A.IsAppvStaff = 0) OR (A.IsAppvStaff = 1 AND A.Status_AppvStaff = 1))
+                        AND ((A.IsAppvChief = 0) OR (A.IsAppvChief = 1 AND A.Status_AppvChief = 1))
+                        AND ((A.IsAppvAsstManager = 0) OR (A.IsAppvAsstManager = 1 AND A.Status_AppvAsstManager = 1))
+                        AND ((A.IsAppvManager) = 0 OR (A.IsAppvManager = 1 AND A.Status_AppvManager = 1))
+                        AND ((A.IsAppvSeniorManager) = 0 OR (A.IsAppvSeniorManager = 1 AND A.Status_AppvSeniorManager = 1))
+                        AND ((A.IsAppvGeneralManager) = 0 OR (A.IsAppvGeneralManager = 1 AND A.Status_AppvGeneralManager = 1))
+                    )
+                )";
+
+        // Total data tanpa filter pencarian
         $totalData = $this->db->query($sql)->num_rows();
+
+        // 2. Menambahkan Filter Pencarian (Search)
         if (!empty($requestData['search']['value'])) {
-            $sql .= " AND (TAccCashBookReq_Header.CBReq_No LIKE '%" . $requestData['search']['value'] . "%' ";
-            $sql .= " OR First_Name LIKE '%" . $requestData['search']['value'] . "%' ";
-            $sql .= " OR Document_Number LIKE '%" . $requestData['search']['value'] . "%' ";
-            $sql .= " OR Document_Date LIKE '%" . $requestData['search']['value'] . "%' ";
-            $sql .= " OR TAccCashBookReq_Header.Currency_Id LIKE '%" . $requestData['search']['value'] . "%' ";
-            $sql .= " OR Descript LIKE '%" . $requestData['search']['value'] . "%' ";
-            $sql .= " OR CBReq_Status LIKE '%" . $requestData['search']['value'] . "%' ";
-            $sql .= " OR Amount LIKE '%" . $requestData['search']['value'] . "%') ";
+            // Menggunakan escape_like_str untuk mencegah SQL Injection
+            $searchValue = $this->db->escape_like_str($requestData['search']['value']);
+
+            // Masalah pada kode asli: Kondisi AND (LIKE OR LIKE) harus ditambahkan setelah WHERE utama
+            // Jika ditambahkan tanpa tanda kurung penutup setelah kondisi OR utama,
+            // logika akan rusak. Di sini, kita menambahkan AND (....) pada akhir kueri.
+
+            $sql .= " AND (H.CBReq_No LIKE '%" . $searchValue . "%' ";
+            $sql .= " OR P.First_Name LIKE '%" . $searchValue . "%' ";
+            $sql .= " OR H.Document_Number LIKE '%" . $searchValue . "%' ";
+            $sql .= " OR H.Document_Date LIKE '%" . $searchValue . "%' ";
+            $sql .= " OR H.Currency_Id LIKE '%" . $searchValue . "%' ";
+            $sql .= " OR H.Descript LIKE '%" . $searchValue . "%' ";
+            $sql .= " OR H.CBReq_Status LIKE '%" . $searchValue . "%' ";
+            $sql .= " OR H.Amount LIKE '%" . $searchValue . "%') ";
         }
-        //----------------------------------------------------------------------------------
+
+        // Total data setelah filter pencarian
         $totalFiltered = $this->db->query($sql)->num_rows();
-        $sql .= " ORDER BY $order $dir OFFSET " . $requestData['start'] . " ROWS FETCH NEXT " . $requestData['length'] . " ROWS ONLY ";
+
+        // 3. Menambahkan Urutan dan Batasan (Pagination)
+        $sql .= " ORDER BY $order $dir OFFSET " . (int)$requestData['start'] . " ROWS FETCH NEXT " . (int)$requestData['length'] . " ROWS ONLY";
+
         $query = $this->db->query($sql);
         $data = array();
-        foreach ($query->result_array() as $row) {
-            $nestedData = array();
-            $nestedData['CBReq_No'] = $row['CBReq_No'];
-            $nestedData['Type'] = $row['Type'];
-            $nestedData['Document_Date'] = $row['Document_Date'];
-            $nestedData['Acc_ID'] = $row['Acc_ID'];
-            $nestedData['Descript'] = $row['Descript'];
-            $nestedData['Document_Number'] = $row['Document_Number'];
-            $nestedData['Amount'] = $row['Amount'];
-            $nestedData['baseamount'] = $row['baseamount'];
-            $nestedData['curr_rate'] = $row['curr_rate'];
-            $nestedData['Approval_Status'] = $row['Approval_Status'];
-            $nestedData['CBReq_Status'] = $row['CBReq_Status'];
-            $nestedData['Paid_Status'] = $row['Paid_Status'];
-            $nestedData['Creation_DateTime'] = $row['Creation_DateTime'];
-            $nestedData['Created_By'] = $row['Created_By'];
-            $nestedData['First_Name'] = $row['Created_By_Name'];
-            $nestedData['Last_Update'] = $row['Last_Update'];
-            $nestedData['Update_By'] = $row['Update_By'];
-            $nestedData['Currency_Id'] = $row['Currency_Id'];
-            $nestedData['Approve_Date'] = $row['Approve_Date'];
-            $nestedData['UserDivision'] = $row['UserDivision'];
 
+        foreach ($query->result_array() as $row) {
+            $nestedData = array(
+                'CBReq_No' => $row['CBReq_No'],
+                'Type' => $row['Type'],
+                'Document_Date' => $row['Document_Date'],
+                'Acc_ID' => $row['Acc_ID'],
+                'Descript' => $row['Descript'],
+                'Document_Number' => $row['Document_Number'],
+                'Amount' => $row['Amount'],
+                'baseamount' => $row['baseamount'],
+                'curr_rate' => $row['curr_rate'],
+                'Approval_Status' => $row['Approval_Status'],
+                'CBReq_Status' => $row['CBReq_Status'],
+                'Paid_Status' => $row['Paid_Status'],
+                'Creation_DateTime' => $row['Creation_DateTime'],
+                'Created_By' => $row['Created_By'],
+                'First_Name' => $row['Created_By_Name'],
+                'Last_Update' => $row['Last_Update'],
+                'Update_By' => $row['Update_By'],
+                'Currency_Id' => $row['Currency_Id'],
+                'Approve_Date' => $row['Approve_Date'],
+                'UserDivision' => $row['UserDivision'] // Tambahkan kolom baru
+            );
             $data[] = $nestedData;
         }
-        //----------------------------------------------------------------------------------
+
+        // Output JSON DataTables
         $json_data = array(
             "draw" => intval($requestData['draw']),
             "recordsTotal" => intval($totalData),
             "recordsFiltered" => intval($totalFiltered),
             "data" => $data,
         );
-        //----------------------------------------------------------------------------------
+
         echo json_encode($json_data);
     }
 }
