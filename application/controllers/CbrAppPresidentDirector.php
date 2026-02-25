0e3617ce78a279dff13ef1b3e0198dcb43bf6ca6
yanamaulana <51176753+yanamaulana@users.noreply.github.com>
2026-02-25 08:38:25 +0700

    Update CbrAppPresidentDirector.php

--- application/controllers/CbrAppPresidentDirector.php
+++ application/controllers/CbrAppPresidentDirector.php
@@ -172,7 +172,8 @@ class CbrAppPresidentDirector extends CI_Controller
             $sql .= " OR Document_Date LIKE '%" . $requestData['search']['value'] . "%' ";
             $sql .= " OR TAccCashBookReq_Header.Currency_Id LIKE '%" . $requestData['search']['value'] . "%' ";
             $sql .= " OR Descript LIKE '%" . $requestData['search']['value'] . "%' ";
-            $sql .= " OR CBReq_Status LIKE '%" . $requestData['search']['value'] . "%' ";
+            $sql .= " OR UserDivision LIKE '%" . $requestData['search']['value'] . "%' ";
+            $sql .= " OR First_Name LIKE '%" . $requestData['search']['value'] . "%' ";
             $sql .= " OR Amount LIKE '%" . $requestData['search']['value'] . "%') ";
         }
         //----------------------------------------------------------------------------------
